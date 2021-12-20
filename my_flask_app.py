87a8a7298cc4f8ec08bb29f96b54d266f989b260
thiswind <thiswind@gmail.com>
2021-12-20 12:29:03 +0000

    随便写点啥

--- my_flask_app.py
+++ my_flask_app.py
@@ -1,11 +1,31 @@
 from flask import Flask
+from flask import render_template
+from flask import request
 
 app = Flask(__name__)
 
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+@app.route("/", methods=['POST', 'GET'])
+def index():
+    return render_template('default.jinja2')
+
+
+@app.route("/echo.php", methods=['POST', 'GET'])
+def echo():
+    msg = request.form.get('msg')
+
+    return render_template('default.jinja2', MSG=msg)
+
+
+@app.route("/<a>/<b>")
+def hello_world(a, b):
+
+    a = int(a)
+    b = int(b)
+
+    one_message = f'{a} + {b} = {a + b}'
+
+    return render_template('default.jinja2', MSG=one_message)
 
 
 if __name__ == '__main__':
